1cc8f0f050787fb78a834ebacda6bba8e8746815
Tomas Zwolinski <toza21@student.bth.se>
2022-04-01 14:10:37 +0200

    kmom01 Done

---
+++ src/Controller/HomeControllerTwig.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeControllerTwig extends AbstractController
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function home(): Response
+    {
+
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about_page")
+     */
+    public function about(): Response
+    {
+
+        return $this->render('about.html.twig');
+    }
+
+    /**
+     * @Route("/report", name="report_page")
+     */
+    public function report(): Response
+    {
+
+        return $this->render('report.html.twig');
+    }
+}
