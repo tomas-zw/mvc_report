df6c6522095b900e6041c429620ddcff0ebd8bfc
Tomas Zwolinski <toza21@student.bth.se>
2022-05-10 19:44:19 +0200

    trying to add chartjs wihout breaking everything

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,15 +5,42 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class ProjectController extends AbstractController
 {
     /**
      * @Route("/proj", name="project")
      */
-    public function project(): Response
+    public function project(ChartBuilderInterface $chartBuilder): Response
     {
-        return $this->render('project/index.html.twig');
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+        return $this->render('project/index.html.twig', [
+            'chart' => $chart
+        ]);
     }
 
     /**
