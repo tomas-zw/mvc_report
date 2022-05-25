624d7c730bc4c60817b7135b1e70e05103112548
Tomas Zwolinski <toza21@student.bth.se>
2022-05-26 01:01:08 +0200

    add tests for TexasRules

--- tests/Card/CardTest.php
+++ tests/Card/CardTest.php
@@ -16,6 +16,7 @@ class CardTest extends TestCase
     public function testCreateDice()
     {
         $value = '5';
+        $intValue = 5;
         $suit = 'clubs';
         $card = new Card($value, $suit);
         $this->assertInstanceOf("\App\Card\Card", $card);
@@ -25,5 +26,56 @@ class CardTest extends TestCase
 
         $resSuit = $card->getColor();
         $this->assertSame($resSuit, $suit);
+
+        $resValueInt = $card->getValueAsInt();
+        $this->assertSame($resValueInt, $intValue);
+    }
+
+    /**
+    * Construct object and verify that the object returns correct value
+    * for A, K, Q, and J
+     */
+    public function testJtoA()
+    {
+        $suit = 'clubs';
+
+        $jack = 'J';
+        $jackInt = 11;
+
+        $queen = 'Q';
+        $queenInt = 12;
+
+        $king = 'K';
+        $kingInt = 13;
+
+        $ace = 'A';
+        $aceInt = 14;
+
+        $card = new Card($jack, $suit);
+        $this->assertInstanceOf("\App\Card\Card", $card);
+
+        $resValueInt = $card->getValueAsInt();
+        $this->assertSame($resValueInt, $jackInt);
+
+        $card = new Card($queen, $suit);
+        $this->assertInstanceOf("\App\Card\Card", $card);
+
+        $resValueInt = $card->getValueAsInt();
+        $this->assertSame($resValueInt, $queenInt);
+
+        $card = new Card($king, $suit);
+        $this->assertInstanceOf("\App\Card\Card", $card);
+
+        $resValueInt = $card->getValueAsInt();
+        $this->assertSame($resValueInt, $kingInt);
+
+        $card = new Card($ace, $suit);
+        $this->assertInstanceOf("\App\Card\Card", $card);
+
+        $resValueInt = $card->getValueAsInt();
+        $this->assertSame($resValueInt, $aceInt);
+
+
+
     }
 }
---
+++ tests/Card/TexasRulesTest.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class TexasRules.
+ */
+class TexasRulesTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties.
+     */
+    public function testCreateTexasRules()
+    {
+        $value = ['5', '5', '5', '4', '2', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'spades', 'spades',  'spades', 'hearts'];
+        $hand = array();
+        for ($i = 0; $i < count($value); $i++) {
+            $hand[] = new Card($value[$i], $suit[$i]);
+
+        }
+        $rules = new TexasRules($hand, $hand);
+        $this->assertInstanceOf("\App\Card\TexasRules", $rules);
+
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean 
+    * for Quads vs Three of a kind.
+     */
+    public function testQuads()
+    {
+        $valuePlayer = ['5', '5', '5', '5', '2', '8', '7'];
+        $valuedealer = ['5', '5', '5', '6', '2', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $suit[$i]);
+            $handDealer[] = new Card($valuedealer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean
+    * for Full House vs flush
+     */
+    public function testFullHouse()
+    {
+        $valuePlayer = ['5', '5', '5', '2', '2', '8', '7'];
+        $valuedealer = ['5', '5', '5', '6', '2', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $suit[$i]);
+            $handDealer[] = new Card($valuedealer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean
+    * for Flush vs three of kind
+     */
+    public function testFlush()
+    {
+        $valuePlayer = ['5', '5', '5', '2', '3', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'spades',  'spades', 'hearts'];
+        $flush = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $flush[$i]);
+            $handDealer[] = new Card($valuePlayer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean
+    * for three of a kind vs two pairs
+     */
+    public function testThreeOfaKind()
+    {
+        $valuePlayer = ['5', '5', '5', '2', '3', '8', '7'];
+        $valueDealer = ['5', '5', '6', '6', '2', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'spades',  'spades', 'hearts'];
+        $flush = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $suit[$i]);
+            $handDealer[] = new Card($valueDealer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean
+    * for two pairs vs pairs
+     */
+    public function testTwoPairs()
+    {
+        $valuePlayer = ['5', '5', '6', '6', '3', '8', '7'];
+        $valueDealer = ['5', '5', '4', '9', '2', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'spades',  'spades', 'hearts'];
+        $flush = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $suit[$i]);
+            $handDealer[] = new Card($valueDealer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean
+    * for pair vs pair
+     */
+    public function testPairs()
+    {
+        $valuePlayer = ['5', '5', '6', '10', '3', '8', 'A'];
+        $valueDealer = ['5', '5', '6', '10', '3', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'spades',  'spades', 'hearts'];
+        $flush = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $suit[$i]);
+            $handDealer[] = new Card($valueDealer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+    }
+
+    /**
+    * Construct object and verify that the function returns correct boolean
+    * for all equal but player has flush
+     */
+    public function testEqualFlush()
+    {
+        $valuePlayer = ['5', '5', '6', '10', '3', '8', 'A'];
+        $valueDealer = ['5', '5', '6', '10', '3', '8', '7'];
+        $suit = ['clubs', 'clubs', 'clubs', 'clubs', 'spades',  'spades', 'hearts'];
+        $flush = ['clubs', 'clubs', 'clubs', 'clubs', 'clubs',  'spades', 'hearts'];
+        $handPlayer = array();
+        $handDealer = array();
+        for ($i = 0; $i < count($valuePlayer); $i++) {
+            $handPlayer[] = new Card($valuePlayer[$i], $flush[$i]);
+            $handDealer[] = new Card($valuePlayer[$i], $suit[$i]);
+        }
+
+        $rules = new TexasRules($handPlayer, $handDealer);
+        $this->assertTrue($rules->isWinner());
+
+        $rules = new TexasRules($handDealer, $handPlayer);
+        $this->assertNotTrue($rules->isWinner());
+    }
+}
